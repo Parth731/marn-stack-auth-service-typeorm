4fed08a4e26a0543c4810903029249e3fbea9b4b
Parth731 <patelparth31795@gmail.com>
2024-12-29 11:51:05 +0530

    add access middleware

--- src/routes/tenantRouter.ts
+++ src/routes/tenantRouter.ts
@@ -3,9 +3,17 @@ import { tenantCreate } from '../controllers/TenantsController';
 import tenantsValidator from '../Validator/tenants-validator';
 import { validate } from '../Validator/ValidationChain';
 import authenticate from '../middlewares/authenticate';
+import { Roles } from '../types';
+import { canAccess } from '../middlewares/canAccess';
 
 const router = express.Router();
 
-router.post('/', authenticate, validate(tenantsValidator), tenantCreate);
+router.post(
+  '/',
+  authenticate,
+  canAccess([Roles.ADMIN]),
+  validate(tenantsValidator),
+  tenantCreate,
+);
 
 export default router;
