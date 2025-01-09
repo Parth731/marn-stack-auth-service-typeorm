cfb3dc9ca84396bc03098eef60a4864a37233d6b
Jawaliya123 <parthdangroshiya1895@gmail.com>
2025-01-10 01:31:39 +0530

    update database env

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -10,13 +10,13 @@ config({
 
 export const AppDataSource = new DataSource({
   type: 'postgres', // PostgreSQL setup
-  // host: configEnv.dbHost,
-  // port: Number(configEnv.dbPort),
-  // username: configEnv.dbUsername,
-  // password: configEnv.dbPassword,
-  // database: configEnv.dbDatabase,
-  url: configEnv.databaseUrl,
-  synchronize: true, //don't use this in production, always keep false
+  host: configEnv.dbHost,
+  port: Number(configEnv.dbPort),
+  username: configEnv.dbUsername,
+  password: configEnv.dbPassword,
+  database: configEnv.dbDatabase,
+  // url: configEnv.databaseUrl,
+  synchronize: false, //don't use this in production, always keep false
   logging: false,
   // entities: [User, RefreshToken],
   entities: ['src/database/entities/*.{ts,js}'], // Add all your entities here
