c94118252f0ca18884304c194e096f9c984393d4
Jawaliya123 <parthdangroshiya1895@gmail.com>
2025-01-10 16:02:16 +0530

    added RDS ssl and updated env variable in pipeline and local

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,27 +1,32 @@
 import 'reflect-metadata';
 import { config } from 'dotenv';
 import path from 'path';
+import fs from 'fs';
 import { DataSource } from 'typeorm';
 import { configEnv } from '../config/config';
 
 config({
-  path: path.join(__dirname, `../../.env.${configEnv.baseUrl || 'dev'}`),
+  path: path.join(__dirname, `../../.env.${configEnv.nodeEnv || 'dev'}`),
 });
 
 export const AppDataSource = new DataSource({
   type: 'postgres', // PostgreSQL setup
-  // host: configEnv.dbHost,
-  // port: Number(configEnv.dbPort),
-  // username: configEnv.dbUsername,
-  // password: configEnv.dbPassword,
-  // database: configEnv.dbDatabase,
-  url: configEnv.databaseUrl,
+  host: configEnv.dbHost,
+  port: Number(configEnv.dbPort),
+  username: configEnv.dbUsername,
+  password: configEnv.dbPassword,
+  database: configEnv.dbDatabase,
+  // url: configEnv.databaseUrl,
   synchronize: true, //don't use this in production, always keep false
   logging: false,
   // entities: [User, RefreshToken],
   entities: ['src/database/entities/*.{ts,js}'], // Add all your entities here
   migrations: ['src/database/migrations/*.{ts,js}'],
   subscribers: [],
+  ssl: {
+    ca: fs.readFileSync(path.join(__dirname, `../rds-ssl/global-bundle.pem`)),
+    rejectUnauthorized: true,
+  },
 });
 
 // export const MongoDataSource = new DataSource({
